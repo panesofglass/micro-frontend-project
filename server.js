00f5f0ab18e52537957934e619d3c7beb59db105
Ryan Riley <ryanriley@live.com>
2022-05-21 20:10:03 -0500

    Milestone 2: fix Vue rendering

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const path = require("path")
 const app = express()
 const port = 3001
 
+// Add mount path to make script type=module work.
+// https://stackoverflow.com/a/50053189/13289748
+express.static.mime.define({"application/javascript": ["js"]})
+
 app.use("/mfe/music/", express.static(path.join(__dirname, "music", "build")))
 app.use("/mfe/welcome/", express.static(path.join(__dirname, "welcome", "dist")))
+app.use("/hello/assets/", express.static(path.join(__dirname, "welcome", "dist", "assets")))
 app.use("/", express.static(path.join(__dirname, "bootstrap", "dist")))
 
 app.all("/*", function (req, res) {
